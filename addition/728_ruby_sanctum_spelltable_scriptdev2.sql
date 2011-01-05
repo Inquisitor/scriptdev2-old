de69f4c5b8b2b6f59d8c9cad1b797f871cd7654d
Inquisitor <inquisitor@kwinta.net>
2011-01-05 23:45:54 +0200

    + Onyx Flamecaller spell target correction. Some minor Saviana English texts corrections. Just reapply SQL.

--- addition/728_ruby_sanctum_spelltable_scriptdev2.sql
+++ addition/728_ruby_sanctum_spelltable_scriptdev2.sql
@@ -115,12 +115,12 @@ INSERT INTO `boss_spell_table` (`entry`, `spellID_N10`, `spellID_N25`, `spellID_
 (39746, 74384, 0, 0, 0, 15000, 0, 0, 0, 25000, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
 (39746, 74398, 0, 0, 0, 45000, 0, 0, 0, 45000, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
 (39746, 39814, 0, 0, 0, 15000, 0, 0, 0, 25000, 0, 0, 0, 40, 0, 0, 9, 0, 0, 0);
--- mob Onyx Falmecaller
+-- mob Onyx Flamecaller
 DELETE FROM `boss_spell_table` WHERE `entry` = 39814;
 INSERT INTO `boss_spell_table` (`entry`, `spellID_N10`, `spellID_N25`, `spellID_H10`, `spellID_H25`, `timerMin_N10`, `timerMin_N25`, `timerMin_H10`, `timerMin_H25`, `timerMax_N10`, `timerMax_N25`, `timerMax_H10`, `timerMax_H25`, `locData_x`, `locData_y`, `locData_z`, `CastType`, `isVisualEffect`, `isBugged`, `textEntry`) VALUES
-(39814, 74392, 0, 0, 0, 10000, 0, 0, 0, 25000, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
-(39814, 74394, 0, 0, 0, 10000, 0, 0, 0, 25000, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
-
+(39814, 74392, 74393, 74392, 74393, 10000, 0, 0, 0, 25000, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+(39814, 74394, 74395, 74394, 74395, 10000, 0, 0, 0, 25000, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0);
+                    
 -- Boss Saviana Ragefire
 DELETE FROM `boss_spell_table` WHERE `entry` = 39747;
 INSERT INTO `boss_spell_table` (`entry`, `spellID_N10`, `spellID_N25`, `spellID_H10`, `spellID_H25`, `timerMin_N10`, `timerMin_N25`, `timerMin_H10`, `timerMin_H25`, `timerMax_N10`, `timerMax_N25`, `timerMax_H10`, `timerMax_H25`,`data1`, `data2`, `data3`, `data4`, `locData_x`, `locData_y`, `locData_z`, `CastType`, `isVisualEffect`, `isBugged`, `textEntry`) VALUES
